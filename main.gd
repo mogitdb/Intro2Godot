16612fdd8b14646df72a63484eae3b1f1c6a9373
MechaOphidian <MechaOphidian@gmail.com>
2024-07-05 09:00:00 -0400

    Exercise on Classes
    GDScript Classes

--- main.gd
+++ main.gd
@@ -1,20 +1,17 @@
 extends Node2D
 
+class_name Main
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	say_hello("Fred")
-	var res = add_nums(4,5)
-	print(res)
+	var a = Animal.new()
+	a.say_age()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-	
-func say_hello(player_name: String) -> void:
-	print("Hello, " + player_name + ", welcome to the course!")
-
-func add_nums(a: int, b: int) -> int:
-	return a + b / 2
