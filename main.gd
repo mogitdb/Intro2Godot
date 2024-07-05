8e0f99fc4901baf72b50ef4ccc957dd481c6ce20
MechaOphidian <MechaOphidian@gmail.com>
2024-07-05 08:02:56 -0400

    Added Variables
    Exercise on GDScript variables

--- main.gd
+++ main.gd
@@ -4,8 +4,18 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hello")
-
+	var score: int = 25
+	var name: String = "John"
+	var speed: float = 78.55464
+	var dead: bool = true
+	var music_volume = null
+	
+	var fs: String = "speed: %.1f" % speed
+	print(fs)
+	fs = "%s %s %.2f %s" % [name, score, speed, dead]
+	print(fs)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("delta:", delta)
+	#print("delta:", delta)
+	pass
