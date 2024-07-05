e1c647408ec8ea260d4ce8a5a7d19abd28868634
MechaOphidian <MechaOphidian@gmail.com>
2024-07-05 08:52:06 -0400

    Exercise on Functions
    GDScript Functions

--- main.gd
+++ main.gd
@@ -3,31 +3,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var person: Dictionary = {
-		"name": "John",
-		"age": 25,
-		"country": "Canada"
-	}
-	print("Name:", person.name)
-	print("Age:", person["age"])
-	print("Country:", person.country)
-	
-	person.age = 30
-	print("person", person)
-	
-	var num_keys = person.size()
-	print("num_keys:", num_keys)
-	
-	person["job"] = "engineer"
-	print("person:", person)
-	
-	var person_has_job = "job" in person
-	print("person_has_job:", person_has_job)
-	
-	for k in person:
-		print(k)
+	say_hello("Fred")
+	var res = add_nums(4,5)
+	print(res)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	pass
+	
+	
+func say_hello(player_name: String) -> void:
+	print("Hello, " + player_name + ", welcome to the course!")
+
+func add_nums(a: int, b: int) -> int:
+	return a + b / 2
