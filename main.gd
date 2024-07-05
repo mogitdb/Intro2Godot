672d58560ff5a5dc782b7dcbc35686cd0baab55f
MechaOphidian <MechaOphidian@gmail.com>
2024-07-05 14:22:47 -0400

    Exercise on Aspect Ratios
    Godot Aspect Ratio setup

--- main.gd
+++ main.gd
@@ -1,24 +1,11 @@
 extends Node2D
 
-#var _health: int
-
-
-var _health: int:
-	get = get_health, set = set_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_health = (100)
-	print(_health)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func get_health() -> int:
-	print("Getting")
-	return _health
-	
-func set_health(value: int) -> void:
-	print("Setting")
-	_health = value
